2d7c7d9cafb4054c78c3dab142765f2867880de7
AllenTigga <98749993+AllenTigga@users.noreply.github.com>
2023-01-06 13:35:53 +0530

    Update RosGUItest_run.py

--- RosGUItest_run.py
+++ RosGUItest_run.py
@@ -11,6 +11,9 @@ class Display_Sensor(tk.Tk):
         super().__init__()
         self.sub = rospy.Subscriber("/chatter", UI_display, self.callback)
         self.rpm_data = tk.IntVar()
+        self.vel_data = tk.IntVar()
+        self.battery_voltage_data = tk.IntVar()
+        self.motor_temp_data = tk.IntVar()
 
         # configure the root window
         self.title('Sensor 1 Data')
@@ -28,18 +31,36 @@ class Display_Sensor(tk.Tk):
         self.style = ttk.Style(self)
         self.style.configure('TLabel', background='black', foreground='red')
         self.rpm_label = ttk.Label(self, text=self.rpm_sensor_data(), font=('Digital-7', 20))
-        self.rpm_label.place(x=250,y=500)
+        self.rpm_label.place(x=100,y=100)
+        self.vel_label = ttk.Label(self, text=self.vel_sensor_data(), font=('Digital-7', 20))
+        self.vel_label.place(x=100,y=200)
+        self.battery_voltage_label = ttk.Label(self, text=self.battery_voltage_sensor_data(), font=('Digital-7', 20))
+        self.battery_voltage_label.place(x=200,y=100)
+        self.motor_temp_label = ttk.Label(self, text=self.motor_temp_sensor_data(), font=('Digital-7', 20))
+        self.motor_temp_label.place(x=200,y=200)
         self.rpm_label.after(1000, self.update)     # schedule an update every 1 second
 
     def callback(self, data):   
         self.rpm_data = data.rpm
+        self.vel_data = data.vel
+        self.battery_voltage_data = data.battery_voltage
+        self.motor_temp_data = data.motor_temp
 
     def rpm_sensor_data(self):
         return self.rpm_data
+    def vel_sensor_data(self):
+        return self.vel_data
+    def battery_voltage_sensor_data(self):
+        return self.battery_voltage_data
+    def motor_temp_sensor_data(self):
+        return self.motor_temp_data
 
     def update(self):
         """ update the label every 1 second """
         self.rpm_label.configure(text=self.rpm_sensor_data())
+        self.vel_label.configure(text=self.vel_sensor_data())
+        self.battery_voltage_label.configure(text=self.battery_voltage_sensor_data())
+        self.motor_temp_label.configure(text=self.motor_temp_sensor_data())
         self.rpm_label.after(1000, self.update)     # schedule another timer
  
 if __name__ == "__main__":
